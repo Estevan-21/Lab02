803b868910ae3037a9a0c44d78b8abbdaa47b967
vdavid30 <spm1dh@gmail.com>
2017-08-21 18:14:24 -0500

    Test de posibles casos de aprovacion realizados

--- GoF-ChainOfResponsabilityExercises/GoF-ChainOfRep-LoanApproval/src/test/java/LoanEvaluatorTest.java
+++ GoF-ChainOfResponsabilityExercises/GoF-ChainOfRep-LoanApproval/src/test/java/LoanEvaluatorTest.java
@@ -1,4 +1,9 @@
 
+import edu.eci.arsw.gof.chainofrep.loanapproval.model.ApplicationDetails;
+import edu.eci.arsw.gof.chainofrep.loanapproval.model.AutomatedLoanEvaluator;
+import java.text.ParseException;
+import static java.time.Clock.system;
+import java.util.Calendar;
 import static junit.framework.Assert.*;
 import org.junit.Test;
 
@@ -14,10 +19,100 @@ import org.junit.Test;
  */
 public class LoanEvaluatorTest {
     @Test
-    public void deberiaRetornarTrue(){
-        assertTrue(true);
+    public void deberiaAporbarPorEdadIntermedia() throws ParseException{
+        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
+        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1980-01-01",
+                "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
+                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
+                "Loan description", "IBM", 30000, 10, 2, 
+                "Officer", "342 SouthLake Av", "", "Yorktown", 
+                "VA", 3242323);
+        
+        String outcome[]=new String[1];
+        assertTrue(!ale.isApplicationDeclined(ad,outcome));
     }
+    @Test
+    public void deberiaNegarPorEdadMenor() throws ParseException{
+        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
+        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "2001-01-01",
+                "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
+                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
+                "Loan description", "IBM", 30000, 10, 2, 
+                "Officer", "342 SouthLake Av", "", "Yorktown", 
+                "VA", 3242323);
+        
+        String outcome[]=new String[1];
+        assertTrue(ale.isApplicationDeclined(ad,outcome));
     
-    
+    }
+    @Test
+    public void deberiaNegarPorEdadMayor() throws ParseException{
+        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
+        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1951-01-01",
+                "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
+                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
+                "Loan description", "IBM", 30000, 10, 2, 
+                "Officer", "342 SouthLake Av", "", "Yorktown", 
+                "VA", 3242323);
+        
+        String outcome[]=new String[1];
+        assertTrue(ale.isApplicationDeclined(ad,outcome));
+    }
+    @Test
+    public void deberiaNegarPorPocaExperiencia() throws ParseException{
+        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
+        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1995-01-01",
+                "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
+                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
+                "Loan description", "IBM", 30000, 0, 4, 
+                "Officer", "342 SouthLake Av", "", "Yorktown", 
+                "VA", 3242323);
+        
+        String outcome[]=new String[1];
+        assertTrue(ale.isApplicationDeclined(ad,outcome));
+
+    }
+    @Test
+    public void deberiaPermitirPorExperiencia() throws ParseException{
+        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
+        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1995-01-01",
+                "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
+                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
+                "Loan description", "IBM", 30000, 1, 0, 
+                "Officer", "342 SouthLake Av", "", "Yorktown", 
+                "VA", 3242323);
+        
+        String outcome[]=new String[1];
+        assertTrue(!ale.isApplicationDeclined(ad,outcome));
+
+    }
+    @Test
+    public void deberiaPermitirPorSalario() throws ParseException{
+        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
+        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1995-01-01",
+                "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
+                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
+                "Loan description", "IBM", 30000, 1, 0, 
+                "Officer", "342 SouthLake Av", "", "Yorktown", 
+                "VA", 3242323);
+        
+        String outcome[]=new String[1];
+        assertTrue(!ale.isApplicationDeclined(ad,outcome));
+
+    }
+    @Test
+    public void deberiaNegarPorSalario() throws ParseException{
+        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
+        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1995-01-01",
+                "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
+                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
+                "Loan description", "IBM", 9999, 1, 0, 
+                "Officer", "342 SouthLake Av", "", "Yorktown", 
+                "VA", 3242323);
+        
+        String outcome[]=new String[1];
+        assertTrue(ale.isApplicationDeclined(ad,outcome));
+
+    }
     
 }
