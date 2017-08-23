6aa52bf3c7b67a1d4dccfa1ccc0a34ff7e86d55b
vdavid30 <spm1dh@gmail.com>
2017-08-22 21:13:19 -0500

    Test mejorados

--- GoF-ChainOfResponsabilityExercises/GoF-ChainOfRep-LoanApproval/src/test/java/LoanEvaluatorTest.java
+++ GoF-ChainOfResponsabilityExercises/GoF-ChainOfRep-LoanApproval/src/test/java/LoanEvaluatorTest.java
@@ -5,6 +5,8 @@ import java.text.ParseException;
 import static java.time.Clock.system;
 import java.util.Calendar;
 import static junit.framework.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /*
@@ -18,175 +20,127 @@ import org.junit.Test;
  * @author hcadavid
  */
 public class LoanEvaluatorTest {
-    @Test
-    public void deberiaAporbarPorEdadIntermedia() throws ParseException{
-        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
-        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1980-01-01",
+    static AutomatedLoanEvaluator ale;
+    static ApplicationDetails ad;
+    static String outcome[];
+    @BeforeClass
+    public static void config(){
+        outcome = new String[1];
+        ale = new AutomatedLoanEvaluator();
+        ad = new ApplicationDetails("Old", "John", "Connor", "1980-01-01",
                 "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
                 "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
                 "Loan description", "IBM", 30000, 10, 2, 
                 "Officer", "342 SouthLake Av", "", "Yorktown", 
-                "VA", 3242323);
-        
-        String outcome[]=new String[1];
-        assertTrue(!ale.isApplicationDeclined(ad,outcome));
+                "VA", 3242323);    
+    }  
+    @Before
+    public  void reConfig(){
+        outcome[0] ="";
+    }
+    @Test
+    public void deberiaAporbarPorEdadIntermedia() throws ParseException{
+        ad.setDob("1995-01-01");
+        ad.setWork_Ex_Year(1);
+        ad.setWork_Ex_Mon(0);
+        ad.setAnn_Sal(40000);
+        assertFalse(ale.isApplicationDeclined(ad,outcome));
     }
     
     @Test
     public void deberiaNegarPorEdadMenor() throws ParseException{
-        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
-        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "2001-01-01",
-                "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
-                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
-                "Loan description", "IBM", 30000, 10, 2, 
-                "Officer", "342 SouthLake Av", "", "Yorktown", 
-                "VA", 3242323);
-        
-        String outcome[]=new String[1];
+        ad.setDob("2001-01-01");        
         assertTrue(ale.isApplicationDeclined(ad,outcome));
     }
     
     @Test
     public void deberiaNegarPorEdadMayor() throws ParseException{
-        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
-        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1951-01-01",
-                "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
-                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
-                "Loan description", "IBM", 30000, 10, 2, 
-                "Officer", "342 SouthLake Av", "", "Yorktown", 
-                "VA", 3242323);
-        
-        String outcome[]=new String[1];
+        ad.setDob("1951-01-01");
         assertTrue(ale.isApplicationDeclined(ad,outcome));
     }
     
     @Test
     public void deberiaNegarPorPocaExperiencia() throws ParseException{
-        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
-        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1995-01-01",
-                "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
-                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
-                "Loan description", "IBM", 30000, 0, 4, 
-                "Officer", "342 SouthLake Av", "", "Yorktown", 
-                "VA", 3242323);
-        
-        String outcome[]=new String[1];
+        ad.setWork_Ex_Mon(4);
+        ad.setWork_Ex_Year(0);
         assertTrue(ale.isApplicationDeclined(ad,outcome));
-    }
-    
+    }   
     @Test
     public void deberiaPermitirPorExperiencia() throws ParseException{
-        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
-        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1995-01-01",
-                "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
-                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
-                "Loan description", "IBM", 30000, 1, 0, 
-                "Officer", "342 SouthLake Av", "", "Yorktown", 
-                "VA", 3242323);
-        
-        String outcome[]=new String[1];
-        assertTrue(!ale.isApplicationDeclined(ad,outcome));
-    }
-    
+        ad.setDob("1995-01-01");
+        ad.setWork_Ex_Year(1);
+        ad.setWork_Ex_Mon(0);
+        assertFalse(ale.isApplicationDeclined(ad,outcome));
+    }    
     @Test
     public void deberiaPermitirPorSalario() throws ParseException{
-        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
-        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1995-01-01",
-                "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
-                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
-                "Loan description", "IBM", 30000, 1, 0, 
-                "Officer", "342 SouthLake Av", "", "Yorktown", 
-                "VA", 3242323);
-        
-        String outcome[]=new String[1];
-        assertTrue(!ale.isApplicationDeclined(ad,outcome));
+        ad.setAnn_Sal(40000);
+        assertFalse(ale.isApplicationDeclined(ad,outcome));
     }
     
     @Test
     public void deberiaNegarPorSalario() throws ParseException{
-        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
-        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1995-01-01",
-                "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
-                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
-                "Loan description", "IBM", 9999, 1, 0, 
-                "Officer", "342 SouthLake Av", "", "Yorktown", 
-                "VA", 3242323);
-        
-        String outcome[]=new String[1];
+        ad.setAnn_Sal(9999);
         assertTrue(ale.isApplicationDeclined(ad,outcome));
     }
     
+    
     @Test
     public void comprobandoScore1() throws ParseException{
-        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
-        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1995-01-01",
-                "Single", "11101", 0, "Credit Card", "742 de Evergreen Terrace", 
-                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
-                "Loan description", "IBM", 9999, 1, 0, 
-                "Officer", "342 SouthLake Av", "", "Yorktown", 
-                "VA", 3242323);        
+        ad.setDob("1995-01-01");
+        ad.setLoan_Purpose("Credit Card");
+        ad.setAnn_Sal(9999);
+        ad.setWork_Ex_Year(1);
+        ad.setWork_Ex_Mon(0);
         assertEquals(310.4725170160689,ale.getScore(ad));
     }
-   
+    
     @Test
     public void comprobandoScore2() throws ParseException{
-        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
-        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1995-01-01",
-                "Single", "11101", 0, "Car Loan", "742 de Evergreen Terrace", 
-                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
-                "Loan description", "IBM", 9999, 1, 0, 
-                "Officer", "342 SouthLake Av", "", "Yorktown", 
-                "VA", 3242323);                
+        ad.setDob("1995-01-01");
+        ad.setLoan_Purpose("Car Loan");
+        ad.setAnn_Sal(9999);
+        ad.setWork_Ex_Year(1);
+        ad.setWork_Ex_Mon(0);                     
         assertEquals(309.10138911302874,ale.getScore(ad));        
     }
-    
+
     @Test
     public void comprobandoScore3() throws ParseException{
-        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
-        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1995-01-01",
-                "Single", "11101", 0, "Debt Consolidation", "742 de Evergreen Terrace", 
-                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
-                "Loan description", "IBM", 9999, 1, 0, 
-                "Officer", "342 SouthLake Av", "", "Yorktown", 
-                "VA", 3242323);  
-        assertEquals(308.85537511089655,ale.getScore(ad));
-        
-        
+        ad.setDob("1995-01-01");
+        ad.setLoan_Purpose("Debt Consolidation");
+        ad.setAnn_Sal(9999);
+        ad.setWork_Ex_Year(1);
+        ad.setWork_Ex_Mon(0);  
+        assertEquals(308.85537511089655,ale.getScore(ad));     
     }
-    
+        
     @Test
     public void comprobandoScore4() throws ParseException{
-        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
-        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1995-01-01",
-                "Single", "11101", 0, "Educational Loan", "742 de Evergreen Terrace", 
-                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
-                "Loan description", "IBM", 9999, 1, 0, 
-                "Officer", "342 SouthLake Av", "", "Yorktown", 
-                "VA", 3242323);                
+        ad.setDob("1995-01-01");
+        ad.setLoan_Purpose("Educational Loan");
+        ad.setAnn_Sal(9999);
+        ad.setWork_Ex_Year(1);
+        ad.setWork_Ex_Mon(0);              
         assertEquals(308.117495029021,ale.getScore(ad));
     }
-    
+ 
     @Test
     public void comprobandoScore5() throws ParseException{
-        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
-        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1995-01-01",
-                "Single", "11101", 0, "Home Improvement Loan", "742 de Evergreen Terrace", 
-                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
-                "Loan description", "IBM", 9999, 1, 0, 
-                "Officer", "342 SouthLake Av", "", "Yorktown", 
-                "VA", 3242323);        
+        ad.setDob("1995-01-01");
+        ad.setLoan_Purpose("Home Improvement Loan");
+        ad.setAnn_Sal(9999);
+        ad.setWork_Ex_Year(1);
+        ad.setWork_Ex_Mon(0);          
         assertEquals(309.5231890083958,ale.getScore(ad));
-    }
-    
+    }  
     @Test
     public void comprobandoScore6() throws ParseException{
-        AutomatedLoanEvaluator ale = new AutomatedLoanEvaluator();
-        ApplicationDetails ad = new ApplicationDetails("Old", "John", "Connor", "1995-01-01",
-                "Single", "11101", 0, "House Loan", "742 de Evergreen Terrace", 
-                "", "Springfield", "CA", 0, 0, 0, 0, "john123@hotmail.com", 
-                "Loan description", "IBM", 9999, 1, 0, 
-                "Officer", "342 SouthLake Av", "", "Yorktown", 
-                "VA", 3242323);        
+        ad.setDob("1995-01-01");
+        ad.setLoan_Purpose("House Loan");
+        ad.setAnn_Sal(9999);
+        ad.setWork_Ex_Year(1);
+        ad.setWork_Ex_Mon(0);       
         assertEquals(308.4336993429769,ale.getScore(ad));
     }
     
